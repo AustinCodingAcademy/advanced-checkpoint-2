ed4981ad0ee2b014eaddea46f2e9108071d968d9
Matt <mmhuntsberry@gmail.com>
2017-12-23 13:40:28 -0600

    moved material-css to css folder so it can be seen

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import "materialize-css/dist/css/materialize.min.css";
+import "../public/css/materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import AppContainer from "./AppContainer";
