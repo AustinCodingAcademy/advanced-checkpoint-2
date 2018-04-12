32efcb97162e8d81b4ba46789ca89e6e87a481bc
Aaron Austin <aaronaustin@Aarons-MacBook-Pro.local>
2018-04-12 18:54:12 -0500

    checkpoint-2 commit2

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import {
   Route,
   Switch
  } from "react-router-dom";
+import CreateQuestionContainer from "./containers/CreateQuestionContainer";
 
 class App extends Component {
   constructor() {
@@ -29,6 +30,7 @@ class App extends Component {
         Client
         <QuestionContainer />
         <QuestionsContainer />
+        <CreateQuestionContainer />
 
 
         {/* <Link to="/questions"><Button>List Questions</Button></Link> */}
