4ec1d53530b10ab9d9c518b723792e30883a1421
Aaron Austin <aaronaustin@Aarons-MacBook-Pro.local>
2018-04-16 16:10:42 -0500

    checkpoint-2 commit4

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 const app = express();
 
-mongoose.set("debug", true);
+// mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/checkpoint2");
-// mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds129394.mlab.com:29394/austin-prod");
+// mongoose.connect("mongodb://localhost/checkpoint2");
+mongoose.connect("mongodb://aaronaustintx:airhead88@ds129394.mlab.com:29394/austin-prod");
 
 // mongodb://<dbuser>:<dbpassword>@ds129394.mlab.com:29394/austin-prod
-
+ 
 const port = process.env.PORT || 3001;
+app.use(require('./routes/QuestionRoutes'))
 app.listen(port, () => {
   console.log(`Listening on port:${port}`);
 });
